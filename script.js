2ef75913ee414afd63556d66e00d72bab2d2b6c9
erondmx <66035050+erondmx@users.noreply.github.com>
2023-12-28 09:52:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('calcular').addEventListener('click', function() {
     var lucroDesejado = parseFloat(document.getElementById('lucroDesejado').value);
     var odd = parseFloat(document.getElementById('odd').value);
     if (!isNaN(perdaAnterior) && !isNaN(lucroDesejado) && !isNaN(odd) && odd > 1) {
-        var valorApostar = ((perdaAnterior + lucroDesejado) / (odd - 1)).toFixed(2);
+        var valorApostar = ((perdaAnterior + lucroDesejado) / odd).toFixed(2);
         document.getElementById('resultado').innerText = 'Valor a apostar: R$ ' + valorApostar;
     } else {
         document.getElementById('resultado').innerText = 'Por favor, insira valores válidos.';
